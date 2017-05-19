a33daab74cfd0a14cce080ddf3bad568053ae45a
neha <support@maalgaadi.net>
2017-05-19 14:12:30 +0530

    first commit

---
+++ firstlibrary/src/main/java/com/neha/firstlibrary/Login.java
@@ -0,0 +1,23 @@
+package com.neha.firstlibrary;
+
+import android.content.Context;
+
+/**
+ * Created by neha on 19/5/17.
+ */
+
+public class Login {
+    private Context context;
+
+
+    public Login(Context context) {
+        this.context = context;
+    }
+
+    public void show() {
+        System.out.println("Enter Number");
+
+    }
+
+
+}
